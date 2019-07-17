37e61af9660bb2f0d5868d34ea2018684de70cbd
lrog <lucas.roguski@gmail.com>
2019-07-17 09:42:49 +0100

    working on config

--- src/main/java/tika/processor/TikaProcessorConfig.java
+++ src/main/java/tika/processor/TikaProcessorConfig.java
@@ -3,20 +3,48 @@ package tika.processor;
 import lombok.Data;
 import org.apache.tika.config.TikaConfig;
 import org.apache.tika.parser.ocr.TesseractOCRConfig;
+import org.apache.tika.parser.pdf.PDFParserConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Data
+@Configuration
+@PropertySource("classpath:tika-config.properties")
 public class TikaProcessorConfig {
+
+    // Apache Tika configuration and used parsers
+    //
     private TikaConfig tikaConfig;
+
     private TesseractOCRConfig tesseractConfig;
 
-    // TODO: externalize the parameters
-    /*
-    public TikaProcessorConfig() throws IOException, SAXException, TikaException  {
-        tikaConfig = new TikaConfig(this.getClass().getClassLoader()
-                .getResourceAsStream("tika-config.xml"));
-    }
-    */
+    private PDFParserConfig pdfTextOnlyConfig;
+    private PDFParserConfig pdfOcrConfig;
+
+
+    // external configuration parameter values
+    //
+    @Value("${tika.parsers.tesseract-ocr.timeout:120}")
+    private int ocrTimeout;
+
+    @Value("${tika.parsers.tesseract-ocr.apply-rotation:false}")
+    private boolean ocrApplyRotation;
+
+    @Value("${tika.parsers.tesseract-ocr.enable-image-processing:false}")
+    private boolean ocrEnableImageProcessing;
+
+    @Value("${tika.parsers.tesseract-ocr.language:eng}")
+    private String ocrLanguage;
+
+    // strategy can be: "ocr-only"
+    @Value("${tika.parsers.pdf-ocr-parser.ocr-only-strategy:true}")
+    private int pdfOcrOnlyStrategy;
+
+    @Value("${tika.parsers.pdf-ocr-parser.min-text-length:100}")
+    private int pdfMinTextLength;
+
 
     public TikaProcessorConfig() throws Exception {
         tikaConfig = new TikaConfig();
