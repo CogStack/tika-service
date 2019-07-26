ab7eea353561fda41ae7f32c20ec115c97cccf07
lrog <lucas.roguski@gmail.com>
2019-07-26 11:52:43 +0100

    build: hunting for failed tests error cause

--- travis_gradle_build.sh
+++ travis_gradle_build.sh
@@ -5,14 +5,39 @@ set -e
 export PING_SLEEP=30s
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BUILD_OUTPUT=$WORKDIR/build.out
+export TEST_LOG_OUTPUT=$WORKDIR/testlog.out
 
-DUMP_LINES=200000
+DUMP_LINES=-1
 
 touch $BUILD_OUTPUT
+touch $TEST_LOG_OUTPUT
 
-dump_output() {
-   echo Tailing the last $DUMP_LINES lines of output:
-   tail -$DUMP_LINES $BUILD_OUTPUT
+print_log_separator() {
+  echo "----------------------------------------------------------------"
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  echo "----------------------------------------------------------------"
+}
+
+dump_build_output() {
+  if [ "$DUMP_LINES" -eq "-1" ]; then
+    echo "Printing all the build output:"
+    cat $BUILD_OUTPUT 
+  else
+    echo "Tailing the last $DUMP_LINES lines of build output:"
+    tail -$DUMP_LINES $BUILD_OUTPUT
+  fi
+}
+
+dump_test_output() {
+  echo "Printing the tests logger output:"
+  cat $TEST_LOG_OUTPUT
+}
+
+run_build() {
+  ./gradlew build --full-stacktrace --debug 2>&1 | tee >(grep TestEventLogger >> $TEST_LOG_OUTPUT) >> $BUILD_OUTPUT
 }
 
 error_handler() {
@@ -30,10 +55,14 @@ PING_LOOP_PID=$!
 
 # Build Commands
 #./gradlew build --stacktrace --debug  >> $BUILD_OUTPUT 2>&1
-./gradlew build --full-stacktrace --debug  >> $BUILD_OUTPUT
+run_build
 
 # The build finished without returning an error so dump a tail of the output
-dump_output
+print_log_separator
+dump_build_output
+
+print_log_separator
+dump_test_output
 
 # nicely terminate the ping output loop
 kill $PING_LOOP_PID
